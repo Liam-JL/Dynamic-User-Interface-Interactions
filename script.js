f49d17c42b999b39f3f6d306f87049d41d320346
LiamJPOS <paledeepened@gmail.com>
2025-04-14 20:39:40 +0100

    Image carousel buttons and dots function

---
+++ script.js
@@ -0,0 +1,41 @@
+const dotContainer = document.querySelector("[data-dot-container]");
+const slides = dotContainer
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
+
+for(let i = 1; i < slides.children.length; i++) {
+    const dot = document.createElement("button");
+    dot.className = "dot";
+    dotContainer.append(dot);
+}
+
+dotContainer.children[0].dataset.active = true;
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+
+        const activeSlide = slides.querySelector("[data-active]");
+        const activeDot = dotContainer.querySelector("[data-active]")
+
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) {
+            newIndex = slides.children.length - 1;
+        }
+        if (newIndex >= slides.children.length) {
+            newIndex = 0;
+        }
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+        
+
+        dotContainer.children[newIndex].dataset.active = true;
+        delete activeDot.dataset.active;
+    })
+});
+
+//Make dot clickable to change to pic
+//Make images cyclle forward every 5 seconds
